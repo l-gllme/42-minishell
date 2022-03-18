6af7e078dd709ad49e95a5daa8e5b848fe65db72
Louis Guillaume <louisguillaume000@gmail.com>
2022-03-18 14:06:41 +0100

    supp valgrind readline file

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 16:04:06 by lguillau          #+#    #+#             */
-/*   Updated: 2022/03/17 14:46:53 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/03/17 17:25:33 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ void	ft_cd(char *arg)
 	int		i;
 
 	i = 0;
+	if (!arg[0])
+	{
+		str = getenv("HOME");
+		if (chdir(str) == -1)
+			ft_putstr_fd("MiniShell: cd: no such file or directory\n", 2);
+	}
 	if (arg[0] == '~')
 	{
 		str = getenv("HOME");
